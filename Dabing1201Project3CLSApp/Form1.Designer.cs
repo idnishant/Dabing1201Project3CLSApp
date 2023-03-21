21d3f675197a4a5f4ae39052801999a7712a821c
nishant <nishant.mca06@gmail.com>
2023-03-21 20:41:16 +0530

    Add project files.

---
+++ Dabing1201Project3CLSApp/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace Dabing1201Project3CLSApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabSource = new System.Windows.Forms.TabPage();
+            this.optGenerateLog = new System.Windows.Forms.CheckBox();
+            this.txtProcessedFile = new System.Windows.Forms.TextBox();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabDest = new System.Windows.Forms.TabPage();
+            this.btnViewSummary = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lstEvents = new System.Windows.Forms.ListBox();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupEventLog = new System.Windows.Forms.GroupBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.errMessage = new System.Windows.Forms.ErrorProvider(this.components);
+            this.watchDir = new System.IO.FileSystemWatcher();
+            this.icuNotify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuConfigure = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.tabControl1.SuspendLayout();
+            this.tabSource.SuspendLayout();
+            this.tabDest.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errMessage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.watchDir)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabSource);
+            this.tabControl1.Controls.Add(this.tabDest);
+            this.tabControl1.ImageList = this.imageList1;
+            this.tabControl1.Location = new System.Drawing.Point(18, 14);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(856, 467);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabSource
+            // 
+            this.tabSource.Controls.Add(this.optGenerateLog);
+            this.tabSource.Controls.Add(this.txtProcessedFile);
+            this.tabSource.Controls.Add(this.txtSource);
+            this.tabSource.Controls.Add(this.label2);
+            this.tabSource.Controls.Add(this.label1);
+            this.tabSource.ImageIndex = 0;
+            this.tabSource.Location = new System.Drawing.Point(4, 29);
+            this.tabSource.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabSource.Name = "tabSource";
+            this.tabSource.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabSource.Size = new System.Drawing.Size(848, 434);
+            this.tabSource.TabIndex = 0;
+            this.tabSource.Text = "Source options";
+            this.tabSource.UseVisualStyleBackColor = true;
+            this.tabSource.Click += new System.EventHandler(this.tabSource_Click);
+            // 
+            // optGenerateLog
+            // 
+            this.optGenerateLog.AutoSize = true;
+            this.optGenerateLog.Location = new System.Drawing.Point(84, 280);
+            this.optGenerateLog.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.optGenerateLog.Name = "optGenerateLog";
+            this.optGenerateLog.Size = new System.Drawing.Size(299, 24);
+            this.optGenerateLog.TabIndex = 4;
+            this.optGenerateLog.Text = "Generate event log for bed file format";
+            this.optGenerateLog.UseVisualStyleBackColor = true;
+            // 
+            // txtProcessedFile
+            // 
+            this.txtProcessedFile.Location = new System.Drawing.Point(92, 212);
+            this.txtProcessedFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtProcessedFile.Name = "txtProcessedFile";
+            this.txtProcessedFile.Size = new System.Drawing.Size(655, 26);
+            this.txtProcessedFile.TabIndex = 3;
+            this.txtProcessedFile.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtProcessedFile_KeyUp);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(243, 48);
+            this.txtSource.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(504, 26);
+            this.txtSource.TabIndex = 2;
+            this.txtSource.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtSource_KeyUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 168);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(274, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "After Processing, Move source file to :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source Directory";
+            // 
+            // tabDest
+            // 
+            this.tabDest.Controls.Add(this.btnViewSummary);
+            this.tabDest.Controls.Add(this.btnRefresh);
+            this.tabDest.Controls.Add(this.lstEvents);
+            this.tabDest.Controls.Add(this.txtDest);
+            this.tabDest.Controls.Add(this.label3);
+            this.tabDest.Controls.Add(this.groupEventLog);
+            this.tabDest.ImageIndex = 0;
+            this.tabDest.Location = new System.Drawing.Point(4, 29);
+            this.tabDest.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabDest.Name = "tabDest";
+            this.tabDest.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabDest.Size = new System.Drawing.Size(848, 434);
+            this.tabDest.TabIndex = 1;
+            this.tabDest.Text = "Destination Options";
+            this.tabDest.UseVisualStyleBackColor = true;
+            // 
+            // btnViewSummary
+            // 
+            this.btnViewSummary.Location = new System.Drawing.Point(459, 332);
+            this.btnViewSummary.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnViewSummary.Name = "btnViewSummary";
+            this.btnViewSummary.Size = new System.Drawing.Size(142, 35);
+            this.btnViewSummary.TabIndex = 5;
+            this.btnViewSummary.Text = "View Summary";
+            this.btnViewSummary.UseVisualStyleBackColor = true;
+            this.btnViewSummary.Click += new System.EventHandler(this.btnViewSummary_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(234, 332);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(135, 35);
+            this.btnRefresh.TabIndex = 4;
+            this.btnRefresh.Text = "Refresh Log";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // lstEvents
+            // 
+            this.lstEvents.FormattingEnabled = true;
+            this.lstEvents.ItemHeight = 20;
+            this.lstEvents.Location = new System.Drawing.Point(102, 118);
+            this.lstEvents.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstEvents.Name = "lstEvents";
+            this.lstEvents.Size = new System.Drawing.Size(656, 184);
+            this.lstEvents.TabIndex = 2;
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(234, 40);
+            this.txtDest.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.Size = new System.Drawing.Size(534, 26);
+            this.txtDest.TabIndex = 1;
+            this.txtDest.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtDest_KeyUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 40);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Destination Directory";
+            // 
+            // groupEventLog
+            // 
+            this.groupEventLog.Location = new System.Drawing.Point(86, 91);
+            this.groupEventLog.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupEventLog.Name = "groupEventLog";
+            this.groupEventLog.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupEventLog.Size = new System.Drawing.Size(684, 232);
+            this.groupEventLog.TabIndex = 3;
+            this.groupEventLog.TabStop = false;
+            this.groupEventLog.Text = "Event Log";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "textbox.png");
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(258, 491);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(112, 35);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(450, 491);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 35);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // errMessage
+            // 
+            this.errMessage.ContainerControl = this;
+            // 
+            // watchDir
+            // 
+            this.watchDir.EnableRaisingEvents = true;
+            this.watchDir.Filter = "*.doc";
+            this.watchDir.SynchronizingObject = this;
+            this.watchDir.Created += new System.IO.FileSystemEventHandler(this.watchDir_Created);
+            // 
+            // icuNotify
+            // 
+            this.icuNotify.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Error;
+            this.icuNotify.Text = "notifyIcon1";
+            this.icuNotify.Visible = true;
+            this.icuNotify.DoubleClick += new System.EventHandler(this.icuNotify_DoubleClick);
+            this.icuNotify.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.icuNotify_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuConfigure,
+            this.mnuExit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(258, 101);
+            // 
+            // mnuConfigure
+            // 
+            this.mnuConfigure.Name = "mnuConfigure";
+            this.mnuConfigure.Size = new System.Drawing.Size(257, 32);
+            this.mnuConfigure.Text = "Configure Application";
+            this.mnuConfigure.Click += new System.EventHandler(this.mnuConfigure_Click);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(257, 32);
+            this.mnuExit.Text = "Exit";
+            this.mnuExit.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 585);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabSource.ResumeLayout(false);
+            this.tabSource.PerformLayout();
+            this.tabDest.ResumeLayout(false);
+            this.tabDest.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errMessage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.watchDir)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabSource;
+        private System.Windows.Forms.TabPage tabDest;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.CheckBox optGenerateLog;
+        private System.Windows.Forms.TextBox txtProcessedFile;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnViewSummary;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ListBox lstEvents;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupEventLog;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ErrorProvider errMessage;
+        private System.IO.FileSystemWatcher watchDir;
+        private System.Windows.Forms.NotifyIcon icuNotify;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuConfigure;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
+
---
+++ Dabing1201Project3CLSApp/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Dabing1201Project3CLSApp
+{
+    public partial class Form1 : Form
+    {
+        private Icon m_info= new Icon(SystemIcons.Information, 40, 40);
+        private Icon m_error=new Icon(SystemIcons.Error, 40, 40);
+
+         private Icon m_ready=new Icon(SystemIcons.Application,40,40);
+
+        public Form1()
+        {
+            InitializeComponent();
+            eventLog1.Log = "Application";
+eventLog1.Source = "WinService";
+
+
+        }
+
+        private void tabSource_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSource.Text="D:\\Creative\\Source\\";
+            txtProcessedFile.Text = "D:\\Creative\\Processed\\";
+            txtDest.Text = "D:\\Creative\\Destination\\";
+            optGenerateLog.Checked = true;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(txtSource.Text))
+            {
+                errMessage.SetError(txtSource, "Invalid Source Directory");
+                txtSource.Focus();
+                tabControl1.SelectedTab = tabSource;
+                return;
+            }
+            else
+                errMessage.SetError(txtSource, "");
+            if (!Directory.Exists(txtDest.Text))
+            {
+                errMessage.SetError(txtDest, "Invalid Destination Directory");
+                txtDest.Focus();
+                tabControl1.SelectedTab = tabDest;
+                return;
+            }
+            else
+                errMessage.SetError(txtDest, "");
+            if (!Directory.Exists(txtProcessedFile.Text))
+            {
+                errMessage.SetError(txtProcessedFile, "Invalid Source Directory");
+                txtProcessedFile.Focus();
+                tabControl1.SelectedTab = tabSource;
+                return;
+            }
+            else
+                errMessage.SetError(txtProcessedFile, "");
+            //watch dir
+            watchDir.Path = txtSource.Text; 
+            watchDir.EnableRaisingEvents=true;
+            
+            //notify 
+           icuNotify.Icon = m_ready;
+           icuNotify.Visible = true;
+           this.ShowInTaskbar = false;
+           this.Hide();
+        }
+
+        private void txtSource_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Directory.Exists(txtSource.Text))
+            {
+                txtSource.BackColor = Color.White;
+            }
+            else
+                txtSource.BackColor = Color.Pink;
+        }
+
+        private void txtDest_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Directory.Exists(txtDest.Text))
+            {
+                txtDest.BackColor = Color.White;
+            }
+            else
+                txtDest.BackColor = Color.Pink;
+        }
+
+        private void txtProcessedFile_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Directory.Exists(txtProcessedFile.Text))
+            {
+                txtProcessedFile.BackColor = Color.White;
+            }
+            else
+                txtProcessedFile.BackColor = Color.Pink;
+        }
+
+        private void mnuConfigure_Click(object sender, EventArgs e)
+        {
+            icuNotify.Visible = false;
+            this.ShowInTaskbar = true;
+            this.Show();
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void icuNotify_DoubleClick(object sender, EventArgs e)
+        {
+            icuNotify.Visible = false;
+            this.ShowInTaskbar=true;
+            this.Show();
+        }
+
+        private void watchDir_Created(object sender, FileSystemEventArgs e)
+        {
+            watchDir.EnableRaisingEvents = false;
+             icuNotify.Icon = m_info;
+             icuNotify.Text = "Processed: " + e.Name;
+            // to access the Word application
+            Microsoft.Office.Interop.Word.Application wdApp = new Microsoft.Office.Interop.Word.Application();
+            object optional = System.Reflection.Missing.Value;
+            //writrer for  XML
+            XmlTextWriter xmlTextWriter = new XmlTextWriter(txtDest.Text + "summary.xml", null);
+            try
+            {
+                Microsoft.Office.Interop.Word.Document doc = new Microsoft.Office.Interop.Word.Document();
+                object filename = e.FullPath;
+                doc = wdApp.Documents.Open(ref filename, ref optional, ref optional, ref optional, ref optional, ref optional,
+                    ref optional, ref optional, ref optional, ref optional, ref optional);
+                
+                Microsoft.Office.Interop.Word.Range wdRange;
+                wdRange = doc.Paragraphs[2].Range;//check
+                
+                string strMemo, strAmount;
+                int intParacount;
+                //read memo
+                strMemo = wdRange.Text;
+                strMemo = strMemo.Substring(13, 4);
+
+                intParacount = doc.Paragraphs.Count;
+                intParacount = intParacount - 2;
+                //read 
+                wdRange = doc.Paragraphs[intParacount].Range;
+                Object count = -1;
+                Object wdCharecter = "1";
+
+                wdRange.MoveEnd(ref wdCharecter, ref count);
+                strAmount = wdRange.Text;
+
+                strAmount = strAmount.Substring(21);
+                //write xml
+                xmlTextWriter.Formatting = Formatting.Indented;
+                xmlTextWriter.WriteDocType("Sales", null, null, null);
+                xmlTextWriter.WriteComment("Summary of sales at Creative Learning");
+                xmlTextWriter.WriteStartElement("Sales");
+                xmlTextWriter.WriteStartElement(Convert.ToString(DateTime.Today));
+                xmlTextWriter.WriteElementString("Memo", strMemo);
+                xmlTextWriter.WriteElementString("Amount", strAmount);
+                xmlTextWriter.WriteEndElement();
+                xmlTextWriter.WriteEndElement();
+                icuNotify.Icon = m_ready;
+
+            }
+            catch (Exception ex) {
+                icuNotify.Icon = m_error;
+                icuNotify.Text = "Error in " + e.Name;
+                if (optGenerateLog.Checked == true) {
+                   EventLog.WriteEntry("CrealtiveLearning",e.Name + ": " + ex.Message);
+                }
+            }
+            finally
+            {
+                xmlTextWriter.Flush();
+                xmlTextWriter.Close();//close xml
+
+                wdApp.Quit(ref optional, ref optional, ref optional);
+                wdApp = null;
+                watchDir.EnableRaisingEvents = true;
+
+            }
+            tryAgain:
+            try
+            {
+                File.Move(e.FullPath, txtProcessedFile.Text + e.Name);
+            }
+            catch
+            {
+                goto tryAgain;
+            }
+        }  //end                                                                                                                                         
+
+        private void icuNotify_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            lstEvents.Items.Clear();
+            eventLog1.Log = "Application";
+            eventLog1.MachineName = ".";
+            foreach(EventLogEntry logEntry in eventLog1.Entries)
+            {
+                if (logEntry.Source == "CrealtiveLearning")
+                {
+                    lstEvents.Items.Add(logEntry.Message);
+                }
+            }
+        }
+
+        private void btnViewSummary_Click(object sender, EventArgs e)
+        {
+            StreamReader strRead;
+            try
+            {
+                strRead = new StreamReader(txtDest.Text + "Summary.xml");
+                MessageBox.Show(strRead.ReadToEnd(), txtDest.Text + "Summary.xml", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                strRead.Close();    
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An Error was returned : "+ex.Message+" Please check the destination folder for summary");
+            }
+
+        }
+    }
+}
